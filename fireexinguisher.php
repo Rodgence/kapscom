77d75a6727215773dca2a028ed7eaf1762ff0491
rodgence <rodgencekagaba@yahoo.com>
2024-03-01 09:38:52 +0300

    Fire extinguisher page updated

--- fireexinguisher.php
+++ fireexinguisher.php
@@ -104,7 +104,24 @@
 
 <div class="container pt-5 mt-5">
     <div class="row mt-5">
-        <h1 class="text-center">RodLine</h1>
+      <h1 class="text-center pb-5 pt-5">FIRE EXTINGUISHING SYSTEMS
+</h1>
+       <div class="col-md-6">
+            <p><b>KAPSCOM AFRICA</b> installs fire extinguishing systems using technologies and gases permitted by
+                   standards. This system is ideal for protecting precious and delicate rooms Data processing
+                    centers where precious equipment and data archives are kept. It is also ideal for protecting chemical
+                      materials that may react violently with water. <br><b>KAPSCOM AFRICA</b> can, upon request, integrate fire
+                    extinguishing systems with other electronic detection and automation systems. Our company designs
+                  systems of various designs and sizes depending on the requirements of the customer. Operating
+                  scenarios provided by the customer and the risk analysis carried out by our engineers is also taken into
+                  account during the designing process. The completed project is delivered as a turnkey an optimum
+                  equipment configuration.
+</p>
+       </div>
+
+       <div class="col-md-6">
+        <img src="assets/img/Extinguisher 4[2026] transparent.webp" alt="">
+       </div>
     </div>
 
     <p></p>
