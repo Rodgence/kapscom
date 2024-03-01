8b8ed885db8735fde37aad07c2de3a04024f9d6f
rodgence <rodgencekagaba@yahoo.com>
2024-03-01 10:10:42 +0300

    new files added for fire extinguisher

--- fireexinguisher.php
+++ fireexinguisher.php
@@ -7,8 +7,9 @@
 
 <div class="container pt-5 mt-5">
     <div class="row mt-5">
-      <h1 class="text-center pb-5 pt-5">FIRE EXTINGUISHING SYSTEMS
-</h1>
+      <h1 class="text-center pb-5 pt-5">
+        FIRE EXTINGUISHING SYSTEMS
+      </h1>
        <div class="col-md-6">
             <p><b>KAPSCOM AFRICA</b> installs fire extinguishing systems using technologies and gases permitted by
                    standards. This system is ideal for protecting precious and delicate rooms Data processing
@@ -19,15 +20,36 @@
                   scenarios provided by the customer and the risk analysis carried out by our engineers is also taken into
                   account during the designing process. The completed project is delivered as a turnkey an optimum
                   equipment configuration.
-</p>
+              </p>
        </div>
 
        <div class="col-md-6">
         <img src="assets/img/Extinguisher 4[2026] transparent.webp" alt="">
        </div>
     </div>
+</div>
 
-    <p></p>
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-6">
+      <img src="assets/img/fire.png" alt="" width="100%" height="90%">
+    </div>
+
+    <div class="col-md-6">
+      <p>Fixed detection fire extinguishing systems are available in two categories: electronic
+(detection) and mechanical (extinguishing). By virtue of the Protocol of Montreal signed
+in 1987, it is forbidden to use HALON 1301 gas for fire extinguishing purposes. Though
+the gas is very effective for extinguishing fire, it has very negative effects on the ozone
+layer. This gas was withdrawn from markets gradually before being banned completely in
+1994.</p> 
+
+<p>Since then, the gases FM200, FE 13, Argon and CO2 have been used as means of fire
+extinguishing. However, in the automatic fire extinguishing systems, each gas used in the
+total flooding applications has its own merits and demerits. Therefore, it is a good idea to
+have a good understanding of the requirements in order to make the best choice of
+gases.</p>
+    </div>
+  </div>
 </div>
 
  <!-- include footer -->
