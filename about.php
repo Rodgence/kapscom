acc2c14e734cc4025334c8f4e9308976c4889ec5
rodgence <rodgencekagaba@yahoo.com>
2024-03-01 08:34:04 +0300

    Files renamed from html to php

---
+++ about.php
@@ -0,0 +1,69 @@
+<!-- include header  -->
+<?php
+    include("includes/header.php");
+?>
+
+<div class="container mt-5 pt-5">
+    <div class="col-md-12 mt-5">
+        <h2 class="text-center">About Us</h2>
+        <p class="text-center"><b>KAPSCOM AFRICA</b> was constituted in 2015 in order to provide highest technological solutions and services in the field of
+            Internet Service Provider (ISP), Computer Hardware & Software solutions, and ICT Solutions, Electric, Security and
+            Communications sector for privately owned or governmental leading institutions in TANZANIA.
+        </p> 
+    </div>
+</div>
+
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-md-6 mt-3">
+            <p><b>KAPSCOM AFRICA’s</b> service concept depends on Unconditional Customer Satisfaction. Together with its     professional
+                staff operating in Dar es salaam headquarters and largest cities of Tanzania, KAPSCOM AFRICA is in full control of all of its installed systems located allover Tanzania, trusting its technical staff that consists of highly expertise engineers and technicians in advanced ICT solutions.<br>
+
+                <b>KAPSCOM AFRICA</b> has been the leading counsellor and installer since the commencement of activities in the field of Computer networking, Internet service, Electric, Mechanic, Security and Communications systems in relevance with project design and implementation services; and it provides solutions through applying the latest technological events.</p>
+        </div>
+        <div class="col-md-6">
+            <img src="assets/img/img/about banner.jpg" alt="" width="70%" >
+        </div>
+    </div>
+</div>
+
+<!-- banner -->
+<div class="container">
+  <div class="col-md-12">
+    <img src="assets/img/img/Karibu RodLine Store.png" alt="" width="100%" height="50%">
+  </div>
+</div>
+
+<div class="container mt-3">
+  <div class="row">
+
+    <div class="col-md-6 pt-5 mt-2">
+      <p><b>KAPSCOM AFRICA are good in:</b><br>
+        •Internet Service Provider<br>
+        •Consultancy and project preparation services<br>
+        •Engineering services<br>
+        •Distribution & supplying and Representation services<br>
+        •System & product procurement, and installation services<br>
+        •After sales services: Technical support and maintenance</p>
+    </div>
+
+    <div class="col-md-6">
+        <img src="assets/img/img/abanner.png" alt="" width="90%">
+    </div>
+     
+
+  </div>
+</div>
+
+<div class="container mt-3">
+  <div class="col-md-12">
+    <h2 class="text-center">KAPSCOM AFRICA ORGANIZATIONAL STRUCTURE</h2>
+    <img src="assets/img/kapscom.png" alt="" class="center-block d-block mx-auto" width="90%">
+  </div>
+</div>
+
+
+ <!-- include footer -->
+ <?php
+      include("includes/footer.php");
+ ?>
