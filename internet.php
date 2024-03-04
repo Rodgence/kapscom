a57e75b270bdf8de62c7d8f44c1e7369cf926fbe
rodgence <rodgencekagaba@yahoo.com>
2024-03-04 05:44:48 +0300

    some file has been modified

--- internet.php
+++ internet.php
@@ -7,9 +7,9 @@
 <div class="container mt-5 pt-5">
     <div class="row pt-5">
         <h1 class="text-center">Internet, VoIP and Data Solutions</h1>
-        <div class="col-md-6">
+        <div class="col-md-6 pt-5">
                 <p>
-                KAPSCOM AFRICA LTD is a Premium Corporate Connectivity and Internet Solutions
+                <b>KAPSCOM AFRICA</b> LTD is a Premium Corporate Connectivity and Internet Solutions
                 Provider in Tanzania with a year’s operational experience. It has a large coverage with a
                 huge corporate client base, including banking institutions, ministries, schools,
                 Universities, colleges, international organizations and individuals.<br>
@@ -27,6 +27,51 @@
         </div>
     </div>
 </div>
+
+<!-- another container -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 pt--5">
+            <img src="assets/img/voip.png" alt="" width="80%">
+        </div>
+
+        <div class="col-md-6">
+            <p>
+                        COVERAGE
+            Our International Connectivity is via multiple Undersea Fiber Optic cable out of the
+            country and we have a last mile connectivity / infrastructure on Fiber Optic, VSAT and
+            Wireless. We are covered 5 regions in Tanzania – Dar es salaam, Mwanza, Arusha,
+            Mbeya and Dodoma.
+            </p>
+
+            <p>
+           <b> OTHER SOLUTIONS</b><br>
+                DATA SOLUTION – KAPSCOM AFRICA network is running using Multiprotocol Label
+                Switching (MPLS) which is a mechanism in high-performance telecommunications
+                networks that directs and carries data from one network node to another. <br>MPLS makes
+                it easy to create "virtual links" between distant nodes. It can encapsulate packets of
+                various network protocols. Layer2 / Layer3 MPLS Virtual Private Network.
+                IP VOICE SOLUTION - VoIP Hosted PBX Business Class Features:
+                <ol>
+                    <p><b>></b> FREE local calling</p>
+                </ol>
+                <ol>
+                    <p><b>></b> FREE Zone calling</p>
+                </ol>
+                <ol>
+                    <p><b>></b> FREE intra-office calling</p>
+                </ol>
+                <ol>
+                    <p><b>></b> FREE inter-office calling</p>
+                </ol>
+                <ol>
+                Nationwide long distance as low as 3 cents a minute and also Conference Bridge Service
+                is available.
+            </p>
+        </div>
+    </div>
+</div>
+
 
 
 
