06b86a0ae82dccd4c0dded0b18190035105e4f98
rodgence <rodgencekagaba@yahoo.com>
2024-03-07 11:22:02 +0300

    header links modified

--- includes/header.php
+++ includes/header.php
@@ -51,11 +51,11 @@
           <!-- <li><a class="nav-link scrollto" href="#services">Services</a></li> -->
           <!-- <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li> -->
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="#">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a href="#blog">Blog</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="fireexinguisher.php">Fire Extinguishing Systems</a></li>
-              <li class="dropdown"><a href="computer.php"><span>Computer System Solutions</span></i></a>
+              <li></li>
+              <li class="dropdown"><a href="computer.php"><span>Computer System Solutions</span></i></a> -->
                 <!-- <ul>
                   <li><a href="#">Computer System Solutions</a></li>
                   <li><a href="#">Internet, VoIP and Data Solutions</a></li>
@@ -63,49 +63,52 @@
                   <li><a href="#">Fire & Smoke detection System</a></li>
                   <li><a href="#">Access Control System</a></li>
                 </ul> -->
-              </li>
-              <li><a href="internet.php">Internet, VoIP and Data Solutions</a></li>
-              <li><a href="cctv.php">Closed Circuit TV Systems</a></li>
-              <li><a href="fireandsmoke.php">Fire & Smoke detection System</a></li>
-              <li><a href="accesscontrol.php">Access Control System</a></li>
-              <li><a href="biometric.php">Biometric hand & Fingerprint indentity system</a></li>
-              <li><a href="alarmsystem.php">Burglar Alarm System</a></li>
-              <li><a href="intercomvideo.php">Intercom & Video intercom systems</a></li>
-              <li><a href="publicaddress.php">Public Address System</a></li>
-              <li><a href="dataandvoice.php">Data & Voice System</a></li>
-              <li><a href="tvsatellite.php">TV satellite system</a></li>
-              <li><a href="outdoor.php">Outdoor perimenter intrusion detection systems</a></li>
-              <li><a href="carplate.php">Car plate recognition systems</a></li>
-              <li><a href="x-ray.php">X-ray security screening systems</a></li>
-              <li><a href="aircondition.php">Air Conditioners solutions</a></li>
-              <li><a href="fence.php">Electric fence solutions</a></li>
-            </ul>
-          </li>
-
-          <!-- <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
+              <!-- </li>
               <li>
-                <a href="#">Column 1 link 1</a>
-                <a href="#">Column 1 link 2</a>
-                <a href="#">Column 1 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 2 link 1</a>
-                <a href="#">Column 2 link 2</a>
-                <a href="#">Column 3 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 3 link 1</a>
-                <a href="#">Column 3 link 2</a>
-                <a href="#">Column 3 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 4 link 1</a>
-                <a href="#">Column 4 link 2</a>
-                <a href="#">Column 4 link 3</a>
-              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
             </ul>
           </li> -->
+
+          <li class="dropdown megamenu"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li>
+                  <a href="computer.php">Computer System Solutions</a>
+                  <a href="internet.php">Internet, VoIP & Data solutions</a>
+                  <a href="cctv.php">Closed Circuit TV Systems</a>
+                  <a href="fireandsmoke.php">Fire & Smoke detection System</a>
+                  <a href="alarmsystem.php">Burglar Alarm System</a>
+                  <a href="publicaddress.php">Public Address System</a>
+                  <a href="tvsatellite.php">TV satellite system</a>
+                  <a href="carplate.php">Car plate recognition systems</a>
+                  <a href="fireexinguisher.php">Fire Extinguishing Systems</a>
+                  
+              </li>
+              <li>
+                  <a href="accesscontrol.php">Access Control System</a>
+                  <a href="biometric.php">Biometric & Fingerprint indentity</a>
+                  <a href="fence.php">Electric fence solutions</a>
+                  <a href="intercomvideo.php">Intercom & Video intercom systems</a>
+                  <a href="dataandvoice.php">Data & Voice System</a>
+                  <a href="outdoor.php">Outdoor perimenter intrusion detection systems</a>
+                  <a href="x-ray.php">X-ray security screening systems</a>
+                  <a href="aircondition.php">Air Conditioners solutions</a>
+              </li>
+              
+            </ul>
+          </li>
 
           <li><a class="nav-link scrollto" href="contact.php">Contact</a></li>
           <li><a class="getstarted scrollto" href="about.php">Get Started</a></li>
