e3785a58687699633c182d8991242094634c8043
rodgence <rodgencekagaba@yahoo.com>
2024-03-15 15:29:27 +0300

    Contact page modified

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
     <div class="col-md-12 mt-5">
         <h2 class="text-center">About Us</h2>
         <p class="text-center"><b>KAPSCOM AFRICA</b> was constituted in 2015 in order to provide highest technological solutions and services in the field of
-            Internet Service Provider (ISP), Computer Hardware & Software solutions, and ICT Solutions, Electric, Security and
+            Internet Service Provider (ISP), Computer Hardware & Software solutions, ICT Solutions, Electric fence, ICT Security and
             Communications sector for privately owned or governmental leading institutions in TANZANIA.
         </p> 
     </div>
@@ -19,7 +19,7 @@
             <p><b>KAPSCOM AFRICA’s</b> service concept depends on Unconditional Customer Satisfaction. Together with its     professional
                 staff operating in Dar es salaam headquarters and largest cities of Tanzania, KAPSCOM AFRICA is in full control of all of its installed systems located allover Tanzania, trusting its technical staff that consists of highly expertise engineers and technicians in advanced ICT solutions.<br>
 
-                <b>KAPSCOM AFRICA</b> has been the leading counsellor and installer since the commencement of activities in the field of Computer networking, Internet service, Electric, Mechanic, Security and Communications systems in relevance with project design and implementation services; and it provides solutions through applying the latest technological events.</p>
+                <b>KAPSCOM AFRICA</b> has been the leading counsellor and installer since the commencement of activities in the field of Computer networking, Internet service, Electric fence, ICT Security and Communications systems in relevance with project design and implementation services; and it provides solutions through applying the latest technological events.</p>
         </div>
         <div class="col-md-6">
             <img src="assets/img/img/about banner.jpg" alt="" width="70%" >
@@ -39,10 +39,10 @@
 
     <div class="col-md-6 pt-5 mt-2">
       <p><b>KAPSCOM AFRICA are good in:</b><br>
-        •Internet Service Provider<br>
-        •Consultancy and project preparation services<br>
-        •Engineering services<br>
-        •Distribution & supplying and Representation services<br>
+        •Internet Service Provider (ISP)<br>
+        •ICT Consultancy and project preparation services<br>
+        •ICT Engineering services<br>
+        •ICT distribution & supplying and Representation services<br>
         •System & product procurement, and installation services<br>
         •After sales services: Technical support and maintenance</p>
     </div>
--- index.php
+++ index.php
@@ -45,7 +45,7 @@
               <h2>KAPSCOM AFRICA</h2>
               <p>
                 was constituted in 2015 in order to provide highest technological solutions and services in the field of
-                Internet Service Provider (ISP), Computer Hardware & Software solutions, and ICT Solutions, Electric, Security and
+                Internet Service Provider (ISP), Computer Hardware & Software solutions, ICT Solutions, Electric fence, ICT Security and
                 Communications sector for privately owned or governmental leading institutions in TANZANIA.
               </p>
               <div class="text-center text-lg-start">
@@ -656,9 +656,93 @@
     </section>End Recent Blog Posts Section -->
 
     <!-- contact -->
-    <?php
-          include("contact.php");
-    ?>
+    <div class="container  mt-5 pt-5">
+               <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+
+        <div class="container" data-aos="fade-up">
+  
+          <header class="section-header">
+            <h2>Contact</h2>
+            <p>Contact Us</p>
+          </header>
+  
+          <div class="row gy-4">
+  
+            <div class="col-lg-6">
+  
+              <div class="row gy-4">
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <i class="bi bi-geo-alt"></i>
+                    <h3>Address</h3>
+                    <p><br><a href="https://maps.app.goo.gl/Bre25XrfZ63Bm6TK7" target="_blank">BMTL Building, victoria, New Bagamoyo Road</a></p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <i class="bi bi-telephone"></i>
+                    <h3>Call Us</h3>
+                    <p><a href="tel:+255679520524">+255 679 520 524</a><br><a href="tel:+255762988288">+255 762 988 288</a><br><a href="tel:+255713254553">+255 713 254 553</a></p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <i class="bi bi-envelope"></i>
+                    <h3>Email Us</h3>
+                    <p><a href="mailto:sales@kapscomafrica.co.tz">sales@kapscomafrica.co.tz</a><br></p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <i class="bi bi-clock"></i>
+                    <h3>Open Hours</h3>
+                    <p>Monday - Friday<br>8:00AM - 05:00PM</p>
+                  </div>
+                </div>
+              </div>
+  
+            </div>
+  
+            <div class="col-lg-6">
+              <form action="forms/contact.php" method="post" class="php-email-form">
+                <div class="row gy-4">
+  
+                  <div class="col-md-6">
+                    <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+                  </div>
+  
+                  <div class="col-md-6 ">
+                    <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+                  </div>
+  
+                  <div class="col-md-12">
+                    <input type="text" class="form-control" name="subject" placeholder="Subject" required>
+                  </div>
+  
+                  <div class="col-md-12">
+                    <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+                  </div>
+  
+                  <div class="col-md-12 text-center">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+  
+                    <button type="submit">Send Message</button>
+                  </div>
+  
+                </div>
+              </form>
+  
+            </div>
+  
+          </div>
+  
+        </div>
+  
+      </section><!-- End Contact Section -->
+  </div>
 
   </main><!-- End #main -->
 
